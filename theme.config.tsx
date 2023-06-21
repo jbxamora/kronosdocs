844a7b180c08521e4960020e28330b5e073946f0
jbxamora <jbxamora@icloud.com>
2023-06-21 15:40:55 -0700

    init repo w nextra

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/jbxamora/kronosdocs',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/jbxamora/kronosdocs',
+  footer: {
+    text: 'Kronos',
+  },
+}
+
+export default config
