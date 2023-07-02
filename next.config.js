e7fb2e86d214bf4f9d86cf733b7591d5dd78b601
jbxamora <jbxamora@icloud.com>
2023-07-02 14:51:43 -0700

    updated index file to show

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/': { page: '/web' }, // Set the homepage to the main page of the web section
+      '/aiml': { page: '/aiml' }, // Add a route for the main page of the aiml section
+      // Handle the __nextDefaultLocale query parameter
+      '/': { page: '/web', query: { __nextDefaultLocale: 'en' } },
+      '/aiml': { page: '/aiml', query: { __nextDefaultLocale: 'en' } },
+      ...defaultPathMap,
+    };
+  },
+});
