ed771e6fb89779d6a29b1bfe0253c30de115342d
jbxamora <jbxamora@icloud.com>
2023-07-02 17:49:11 -0700

    update docs to reflect photoport readme

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-});
+})
 
-module.exports = withNextra({
-  exportPathMap: async function (defaultPathMap) {
-    return {
-      '/': { page: '/web' }, // Set the homepage to the main page of the web section
-      '/aiml': { page: '/aiml' }, // Add a route for the main page of the aiml section
-      // Handle the __nextDefaultLocale query parameter
-      '/': { page: '/web', query: { __nextDefaultLocale: 'en' } },
-      '/aiml': { page: '/aiml', query: { __nextDefaultLocale: 'en' } },
-      ...defaultPathMap,
-    };
-  },
-});
+module.exports = withNextra()
